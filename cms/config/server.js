6d2ae050e32c72224f12ec69df6f0ce89961e867
Angel Martinez <angel.martineztrc@gmail.com>
2021-02-13 17:37:09 -0600

    feat(cms): :sparkles:  creating a strapi project

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f12efa959af8ca56f70a7ada809b6c30'),
+    },
+  },
+});
