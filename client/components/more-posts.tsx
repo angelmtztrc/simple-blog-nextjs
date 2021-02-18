f47b8c839b3c2e8e79cfe9e1e0b46c236f569c46
Angel Martinez <angel.martineztrc@gmail.com>
2021-02-17 21:09:54 -0600

    feat(more-posts): :bug:  adding subtitle to the component

--- client/components/more-posts.tsx
+++ client/components/more-posts.tsx
@@ -7,24 +7,27 @@ export type MorePostsProps = {
 };
 const MorePosts = ({ posts }: MorePostsProps) => {
   return (
-    <section className="mt-32 grid grid-cols-2 gap-10">
-      {posts.map((post: Post) => (
-        <article key={post.id} className="flex flex-col px-4">
-          <img
-            className="w-full object-contain"
-            src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${post.image.url}`}
-            alt={post.title}
-          />
-          <div>
+    <section className="mt-32 ">
+      <h2 className="text-3xl font-extrabold mb-5">More Articles</h2>
+      <div className="grid grid-cols-2 gap-10">
+        {posts.map((post: Post) => (
+          <article key={post.id} className="flex flex-col">
+            <img
+              className="w-full object-contain"
+              src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${post.image.url}`}
+              alt={post.title}
+            />
             <div>
-              <h2 className="text-3xl mt-4 text-gray-900">{post.title}</h2>
-              <span className="mt-2 text-gray-600">{toDate(post.published_at)}</span>
+              <div>
+                <h3 className="text-3xl mt-4 text-gray-900">{post.title}</h3>
+                <span className="mt-2 text-gray-600">{toDate(post.published_at)}</span>
+              </div>
+              <p className="text-lg text-gray-900 mt-4">{post.description}</p>
+              <AuthorCard author={post.author} />
             </div>
-            <p className="text-lg text-gray-900 mt-4">{post.description}</p>
-            <AuthorCard author={post.author} />
-          </div>
-        </article>
-      ))}
+          </article>
+        ))}
+      </div>
     </section>
   );
 };
